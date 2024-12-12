7f233d7ddb939eafb11acc1d4280c2029e77bbd5
Yutthaphichai <yutthaphichaikk@gmail.com>
2024-12-12 15:41:41 +0700

    create nacbar

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>เกี่ยวกับเรา</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/products/detail.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>รายละเอียดสินค้า</h1>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
